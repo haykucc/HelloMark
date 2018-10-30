17f483ac878d9ba1f52b44d1b373355205fdc950
WallMich92 <michael.wallner@fau.de>
2018-10-30 23:22:03 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hm-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'HelloMark';
+}
