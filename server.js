0b8710f8195f5eb74cdc27ddc32906e8e94db625
haykucc <mark.czarnetzki@googlemail.com>
2018-11-02 08:31:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist/HelloMark'));
+app.use(express.static(__dirname + '/dist/hello-mark'));
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/HelloMark/index.html');
+  res.status(200).sendFile(__dirname + '/dist/hello-mark/index.html');
 });
 app.listen(process.env.PORT || 8080);
